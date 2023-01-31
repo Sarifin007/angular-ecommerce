ea083867d9e17f404d3bb0dcd641c32c1139dfe2
Sarifin007 <99475443+Sarifin007@users.noreply.github.com>
2023-01-31 11:05:39 +0600

    up
    up

---
+++ angular-ecom-main/src/app/app-routing.module.ts
@@ -0,0 +1,70 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { AuthGuard } from './auth.guard';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { HomeComponent } from './home/home.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { SearchComponent } from './search/search.component';
+import { SellerAddProductComponent } from './seller-add-product/seller-add-product.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { SellerHomeComponent } from './seller-home/seller-home.component';
+import { SellerUpdateProductComponent } from './seller-update-product/seller-update-product.component';
+import { UserAuthComponent } from './user-auth/user-auth.component';
+
+const routes: Routes = [
+  {
+    component: HomeComponent,
+    path: '',
+  },
+  {
+    component: SellerAuthComponent,
+    path: 'seller-auth',
+  },
+  {
+    component: SellerHomeComponent,
+    path: 'seller-home',
+
+  }, {
+    component: SellerAddProductComponent,
+    path: 'seller-add-product',
+
+  }, {
+    component: SellerUpdateProductComponent,
+    path: 'seller-update-product/:id',
+  },
+  {
+    component: SearchComponent,
+    path: 'search/:query'
+  }, {
+    component: ProductDetailsComponent,
+    path: 'details/:productId'
+  }, {
+    component: UserAuthComponent,
+    path: 'user-auth'
+  }, {
+    component: CartPageComponent,
+    path: 'cart-page'
+  },
+  {
+    component: MyOrdersComponent,
+    path: 'my-orders'
+  }
+  , {
+    component: AboutusComponent,
+    path: 'about'
+  }
+  , {
+    component: FeedbackComponent,
+    path: 'feedback'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule { }
